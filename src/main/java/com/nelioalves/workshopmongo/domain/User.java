87ac9f09a939f2b1259695b83e5d1cf210ec6766
Lucas Tavares <t.luuccas@gmail.com>
2020-09-18 15:34:43 -0300

    Conectando o MongoDB com repository

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -2,9 +2,15 @@ package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document()
 public class User  implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	public String id;
 	public String name;
 	public String email;
---
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.nelioalves.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.workshopmongo.Repository.UserRepository;
+import com.nelioalves.workshopmongo.domain.User;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
