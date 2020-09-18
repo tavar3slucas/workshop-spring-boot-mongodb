835b71efe6cba4d194f0a63c72fe771ec8131375
Lucas Tavares <t.luuccas@gmail.com>
2020-09-18 14:29:33 -0300

    Primeiro commit, projeto criado

---
+++ src/test/java/com/nelioalves/workshopmongo/WorkshopgmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopgmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
